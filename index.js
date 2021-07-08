eb01147e1595e66cae93e4469f7f8304461b8518
Shubham Jha <shubham.jha2001m@gmail.com>
2021-07-08 19:52:23 +0530

    Creating Commands & Options

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const program = require('commander')
+const program = require('commander');
 
-program.version('1.0.0').description('Generate Random but Strong Passwords').parse();
+program.version('1.0.0').description('Generate Random but Strong Passwords');
+
+program
+    .option('-l, --length <number>', 'length of password', '8')
+    .option('-s, --save', 'save passwords to passwords.txt')
+    .option('-nn, --no-numbers', 'remove numbers')
+    .option('-ns, --no-symbols', 'remove symbols')
+    .parse();
+
+const { length, save, numbers, symbols } = program.opts();
+
+console.log(program.opts());
